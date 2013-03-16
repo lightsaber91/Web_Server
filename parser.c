697e7e270311d1d80d38bcb6ba0d187cfde17729
Simone Martucci <lightsaber_91@hotmail.it>
2013-03-16 17:39:21 +0100

    Create parser.c
    Questo frammento legge il file di configurazione e crea una struct dove salva le impostazioni del server

---
+++ parser.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+const char *config_path = "/home/lightsaber/devel/Web_Server/config.ini";
+
+struct setting_info {
+  char *ip;
+	int port;
+	char *root_folder;
+};
+
+struct setting_info *parse_config_file ()
+{
+	struct setting_info *s;
+	s = malloc(sizeof(struct setting_info));
+	if(s == NULL) {
+		perror("allocation memory fault");
+		exit(EXIT_FAILURE);
+	}
+
+	size_t n = 50;
+	char *line = NULL;
+
+	FILE *config = fopen(config_path, "r");
+	if(config == NULL) {
+		perror("Fail opening configuration file");
+		exit(EXIT_FAILURE);
+	}
+
+	for(;;) {
+		getline(&line, &n, config);
+		if(strncmp(line, "EOF", 3) == 0)
+			return s;
+		if(strncmp(line, "#", 1) == 0);
+		else {
+			if(strncmp(line, "IP=", 3) == 0) {
+				int i = 3;
+				char ip_t[strlen(line)-i];
+				while(line[i] != ';') {
+					ip_t[i-3] = line[i];
+					i++;
+				}
+				s->ip = ip_t;
+			}
+      if(strncmp(line, "PORT=", 5) == 0) {
+        int i = 5;
+				char port_t[strlen(line)-i];
+        while(line[i] != ';') {
+          port_t[i-5] = line[i];
+          i++;
+        }
+				s->port = atoi(port_t);
+      }
+      if(strncmp(line, "ROOT_FOLDER=", 11) == 0) {
+        int i = 11;
+        char root_t[strlen(line)-i];
+        while(line[i] != ';') {
+          root_t[i-11] = line[i];
+          i++;
+        }
+        s->root_folder = root_t;
+      }
+    }
+	}
+}
