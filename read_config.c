e2c0bc1d92175e625ab8532f66456d7a43f6fdf4
Simone Martucci <lightsaber_91@hotmail.it>
2013-03-22 15:44:34 +0100

    Update read_config.c
    Migliorata la lettura da file di configurazione

--- read_config.c
+++ read_config.c
@@ -2,24 +2,29 @@
 #include <string.h>
 #include <stdlib.h>
 
-const char *config_path = "./config.ini";
+const char *config_path = "./config.txt";
 
 struct setting_info {
 	char *ip;
 	int port;
 	char *root_folder;
+	int max_process;
+	int min_process;
+	int max_thread;
+	int min_thread;
 };
 
 struct setting_info *parse_config_file ()
 {
+	size_t n = 100;
 	struct setting_info *s;
 	s = malloc(sizeof(struct setting_info));
+
 	if(s == NULL) {
 		perror("allocation memory fault");
 		exit(EXIT_FAILURE);
 	}
 
-	size_t n = 50;
 	char *line = NULL;
 
 	FILE *config = fopen(config_path, "r");
@@ -32,36 +37,22 @@ struct setting_info *parse_config_file ()
 		getline(&line, &n, config);
 		if(strncmp(line, "EOF", 3) == 0)
 			return s;
-		if(strncmp(line, "#", 1) == 0)
-			continue;
+		if(strncmp(line, "#", 1) == 0);
 		else {
-			if(strncmp(line, "IP=", 3) == 0) {
-				int i = 3;
-				char ip_t[strlen(line)-i];
-				while(line[i] != ';') {
-					ip_t[i-3] = line[i];
-					i++;
-				}
-				s->ip = ip_t;
-			}
-      			if(strncmp(line, "PORT=", 5) == 0) {
-        			int i = 5;
-				char port_t[strlen(line)-i];
-        			while(line[i] != ';') {
-          				port_t[i-5] = line[i];
-          				i++;
-        			}
+ 			if(strncmp(line, "PORT=", 5) == 0) {
+				strtok(line, "=");
+        			char *port_t = strtok(NULL, ";");
 				s->port = atoi(port_t);
       			}
-      			if(strncmp(line, "ROOT_FOLDER=", 11) == 0) {
-        			int i = 11;
-        			char root_t[strlen(line)-i];
-        			while(line[i] != ';') {
-          				root_t[i-11] = line[i];
-          				i++;
-        			}
-        			s->root_folder = root_t;
+     			else if(strncmp(line, "ROOT_FOLDER=", 12) == 0) {
+				strtok(line, "=");
+				s->root_folder = strtok(NULL, ";");
       			}
+                        else if(strncmp(line, "IP=", 3) == 0) {
+				strtok(line, "=");
+				s->ip = strtok(NULL, ";");
+                        }
+
     		}
 	}
 }
