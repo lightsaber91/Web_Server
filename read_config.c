613715ac1a099a5e1273ccaec02ca75a17dd306a
Simone Martucci <lightsaber_91@hotmail.it>
2013-03-27 15:29:32 +0100

    Update read_config.c

--- read_config.c
+++ read_config.c
@@ -1,58 +1,99 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-const char *config_path = "./config.txt";
+#define conf_file "./server.conf"
 
-struct setting_info {
+struct server_setting {
 	char *ip;
-	int port;
 	char *root_folder;
-	int max_process;
-	int min_process;
-	int max_thread;
+	char *log_path;
+	int port;
+	int log_lvl;
 	int min_thread;
+	int max_thread;
+	int min_process;
+	int max_process;
 };
 
-struct setting_info *parse_config_file ()
-{
-	size_t n = 100;
-	struct setting_info *s;
-	s = malloc(sizeof(struct setting_info));
+void write_parameter(char *line, struct server_setting *s) {
+
+	if ( strncmp(line, "IP=", 3) == 0 ) {
+		strtok(line, "=");
+		s->ip = strtok(NULL, ";");
+	}
+	else if ( strncmp(line, "ROOT_FOLDER=", 12 ) == 0) {
+		strtok(line, "=");
+		s->root_folder = strtok(NULL, ";");
+	}
+	else if ( strncmp(line, "LOG_PATH=", 9 ) == 0 ) {
+
+		if ( strncmp(line, "LOG_PATH=;", 10 ) == 0) {
+			s->log_path = "./server.log";
+		}
+		else {
+			strtok(line, "=");
+			s->log_path = strtok(NULL, ";");
+		}
+	}
+	else if ( strncmp(line, "PORT=", 5) == 0 ) {
+		strtok(line, "=");
+		char *port_t = strtok(NULL, ";");
+		s->port = atoi(port_t);
+	}
+	else if ( strncmp(line, "LOG_LEVEL=", 10) == 0 ) {
+
+		strtok(line, "=");
+		char *lvl_t = strtok(NULL, "\n");
+		if( strncmp(lvl_t, ";", 1) == 0 || strncmp(lvl_t, "NONE", 4) == 0 ) {
+			s->log_lvl = -1;
+		}
+		else if( strncmp(lvl_t, "ERROR", 5) == 0 ) {
+			s->log_lvl = 0;
+		}
+		else if( strncmp(lvl_t, "DEBUG;", 6) == 0 ) {
+			s->log_lvl = 1;
+		}
+
+	}
+
+}
+
+struct server_setting *parse_config_file() {
+
+	struct server_setting *s = malloc(sizeof(struct server_setting));
 
 	if(s == NULL) {
-		perror("allocation memory fault");
+		perror("allocating memory");
 		exit(EXIT_FAILURE);
 	}
 
-	char *line = NULL;
+	size_t n = 100;
 
-	FILE *config = fopen(config_path, "r");
-	if(config == NULL) {
-		perror("Fail opening configuration file");
+	FILE *conf = fopen(conf_file, "r");
+	if(conf == NULL) {
+		perror("config file not found");
 		exit(EXIT_FAILURE);
 	}
 
-	for(;;) {
-		getline(&line, &n, config);
-		if(strncmp(line, "EOF", 3) == 0)
-			return s;
-		if(strncmp(line, "#", 1) == 0);
-		else {
- 			if(strncmp(line, "PORT=", 5) == 0) {
-				strtok(line, "=");
-        			char *port_t = strtok(NULL, ";");
-				s->port = atoi(port_t);
-      			}
-     			else if(strncmp(line, "ROOT_FOLDER=", 12) == 0) {
-				strtok(line, "=");
-				s->root_folder = strtok(NULL, ";");
-      			}
-                        else if(strncmp(line, "IP=", 3) == 0) {
-				strtok(line, "=");
-				s->ip = strtok(NULL, ";");
-                        }
+	while(1) {
 
-    		}
+		char *line = NULL;
+
+		if(getdelim(&line, &n, '\n', conf) == -1) {
+			perror("in getdelim");
+			//exit(EXIT_FAILURE);
+		}
+
+		if(strncmp(line, "EOF" ,3) == 0 ) {
+			break;
+		}
+
+		if( strncmp(line, "#", 1) != 0 ) {
+			write_parameter(line, s);
+		}
+
 	}
+
+	return s;
 }
