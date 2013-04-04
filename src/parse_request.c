099d5bb5e26928b712ade2d640007a7e743ff904
Simone Martucci <lightsaber_91@hotmail.it>
2013-04-04 22:24:13 +0300

    Update and rename parse_request.c to src/parse_request.c

--- src/parse_request.c
+++ src/parse_request.c
@@ -1,18 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct browser_request {
-	char *method;
-	char *file_requested;
-	char *user_agent;
-	char *connection_type;
-	char *http_version;
-	char *host;
-	char *accept_type;
-};
+#include "../lib/parse_request.h"
 
 void parse_request(char *line, struct browser_request *request) {
+
         if(strncmp(line, "User-Agent: ", 12) == 0) {
                 strtok(line, " ");
 		request->user_agent = strtok(NULL, "\n\r");
@@ -32,24 +21,31 @@ void parse_request(char *line, struct browser_request *request) {
 }
 
 void parse_first_line(char *line, struct browser_request *request) {
+
 	request->method = strtok(line, " ");
 	request->file_requested = strtok(NULL, " ");
 	request->http_version = strtok(NULL, "\n\r");
+
 }
 
 struct browser_request *parse_browser_request(char *message) {
+
 	struct browser_request *request;
+
 	request = malloc(sizeof(struct browser_request));
 	if(request == NULL) {
 		perror("memory allocation Failure");
 		exit(EXIT_FAILURE);
 	}
+
 	char *line, *save_pointer;
 	line = strtok_r(message, "\n\r", &save_pointer);
 	parse_first_line(line, request);
+
 	while((line = strtok_r(NULL, "\n\r", &save_pointer)) != NULL) {
 		parse_request(line, request);
 	}
-	printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n", request->method, request->file_requested, request->user_agent, request->connection_type, request->http_version, request->host, request->accept_type);
+
 	return request;
+
 }
