8d72a43f75ab9d63b25e8be945df4088a22df0e1
Simone Martucci <lightsaber_91@hotmail.it>
2013-04-04 22:27:19 +0300

    Create logger.h

---
+++ lib/logger.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+
+int openLogFile(char *path);
+
+void writeConnectionLog(int log, struct browser_request *request);
