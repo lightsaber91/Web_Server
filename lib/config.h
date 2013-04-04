cd1af0928b7a983f10ef4d516c0541ebcc887c2d
Simone Martucci <lightsaber_91@hotmail.it>
2013-04-04 22:26:08 +0300

    Create config.h

---
+++ lib/config.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define conf_file "./server.conf"
+
+typedef struct server_setting {
+  char *ip;
+	char *root_folder;
+	char *home_page;
+	char *log_path;
+	int port;
+	int log_lvl;
+	int min_thread;
+	int max_thread;
+	int min_process;
+	int max_process;
+} server_setting;
+
+void write_parameter(char *line, struct server_setting *s);
+
+struct server_setting *parse_config_file();
