f6c8c81177f362c8ee1b230bee21259b3c2d5c7c
Simone Martucci <lightsaber_91@hotmail.it>
2013-04-04 22:26:37 +0300

    Create parse_request.h

---
+++ lib/parse_request.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct browser_request {
+  char *method;
+	char *file_requested;
+	char *user_agent;
+	char *connection_type;
+	char *http_version;
+	char *host;
+	char *accept_type;
+} browser_request;
+
+void parse_request(char *line, struct browser_request *request);
+
+void parse_first_line(char *line, struct browser_request *request);
+
+struct browser_request *parse_browser_request(char *message);
