cdaaaec4dfcff3df0382ebeb1ffd839873fbe081
Simone Martucci <lightsaber_91@hotmail.it>
2014-05-05 15:40:33 +0200

    Released Under GPL

--- src/logger.c
+++ src/logger.c
@@ -16,7 +16,9 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
-**/#include "../lib/logger.h"
+**/
+
+#include "../lib/logger.h"
 
 /**
  * Open Log File.
